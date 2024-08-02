e5bb639356c909543cad345249a724405f2f3bfb
xiao-k233 <3115216193@qq.com>
2024-08-02 18:40:53 +0800

    feat:捣鼓捣鼓我的小脚本

--- setupenv.sh
+++ setupenv.sh
@@ -4,10 +4,10 @@
 #   我自己用的编译脚本
 #   如果你想用，没问题，自己改改就行了
 echo "\e[1m***************************"
-echo "\e[1mZkernel Build Script"
+echo "\e[1m\e[7mZkernel Build Script"
 echo "\e[1mKernel version:\e[5m"
 make kernelversion
-echo "\e[0m\e[1m***************************"
+echo "\e[0m\e[1m***************************\e[0m"
 export PATH=$PATH:/home/killerz/e9/aarch64-linux-android-4.9/bin
 export CROSS_COMPILE=aarch64-linux-android-
 export ARCH=arm64
