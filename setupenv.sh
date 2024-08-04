418d0bb41ce1e4137c19fd9ac24dd0b261f7b102
xiao-k233 <3115216193@qq.com>
2024-08-04 21:38:28 +0800

    shell

---
+++ setupenv.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+#
+#   我自己用的编译脚本
+#   如果你想用，没问题，自己改改就行了
+echo "\e[1m***************************"
+echo "\e[1m\e[7mZkernel Build Script"
+echo "\e[1mKernel version:\e[5m"
+make kernelversion
+echo "\e[0m\e[1m***************************\e[0m"
+export PATH=$PATH:/home/killerz/e9/aarch64-linux-android-4.9/bin
+export CROSS_COMPILE=aarch64-linux-android-
+export ARCH=arm64
+export GCC_COLORS=auto
+rm -rf ./out
+mkdir out
+make mrproper
+make ARCH=arm64 O=./out merge_kirin980_defconfig
+make ARCH=arm64 O=./out -j20
+cd tools
+./pack_kernerimage_cmd.sh
+ls -lh kernel.img
